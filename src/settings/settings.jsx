b349d3a9f828fbf183c1c9290cf065c072d62316
VuceticBranislav <24853106+VuceticBranislav@users.noreply.github.com>
2024-10-05 23:58:30 +0200

    asd

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -1,8 +1,8 @@
 const settings = {
   footerText: `© 2023-${new Date().getFullYear()} Vucetic Branislav`,
   Path: {
-    CVMechanicalEngineer: "CV-Vucetic-Branislav_ME.pdf",
-    CVSoftwareDeveloper: "CV-Vucetic-Branislav_SD.pdf",
+    CVMechanicalEngineer: "ME/CV-Vucetic-Branislav.pdf",
+    CVSoftwareDeveloper: "SD/CV-Vucetic-Branislav.pdf",
   },
 };
 
